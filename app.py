498f28cbd0250de32a7d35927ae161b27f1e173b
Nopparatz1238 <uiavgcgccggv@gmail.com>
2026-04-02 21:11:27 +0700

    update system dowload mp3 wav

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import yt_dlp
 import questionary
 
+# screen - application
 def screen():
+    youtube_url = questionary.text("youtube url").ask()    
     option = questionary.select(
         "type",
         choices=[
@@ -11,6 +13,37 @@ def screen():
         ],
         pointer="-"
     ).ask()
+    dowloads(youtube_url, option)
 
+# system dowload
+def dowloads(youtube_url, format_type):
+
+    ydl_opts = {
+        'outtmpl': '%(title)s.%(ext)s',
+    }
+
+    if format_type == "mp3" :
+        ydl_opts.update({
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        })
+    elif format_type == "wav" :
+        ydl_opts.update({
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'wav',
+            }],
+        })
+    elif format_type == "mp4" :
+        print("mp4")
+    else :
+        print("invalid type")
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl :
+        ydl.download(youtube_url)
 
 screen()
